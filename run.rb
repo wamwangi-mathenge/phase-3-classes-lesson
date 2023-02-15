7acf97180bcd8f70cbdcbcb14b5a5c593a84958d
Brian <wamwangimathenge@gmail.com>
2023-02-15 12:08:28 +0300

    Instance variables

--- run.rb
+++ run.rb
@@ -1,10 +1,21 @@
+require 'pry'
+
 class Dog
 
-    attr accessor :name, :breed, :age
+    attr_accessor :name, :breed, :age
 
     def initialize(name, breed, age)
         @name = name
         @breed = breed
         @age = age
     end
+
+    def bark
+        puts "Woof Woof!!!"
+    end
 end
+
+dog1 = Dog.new("Bosco", "Chihuahua", 40)
+dog2 = Dog.new("Snoopy", "GS", 15)
+
+binding.pry
