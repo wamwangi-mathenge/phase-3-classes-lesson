4f3204c7495a2e2619a41f8c0d146440c6e8f641
Brian <wamwangimathenge@gmail.com>
2023-02-15 12:19:43 +0300

    Class methods

--- run.rb
+++ run.rb
@@ -4,11 +4,31 @@ class Dog
 
     attr_accessor :name, :breed, :age
 
+    @@alldogs = 0
+
     def initialize(name, breed, age)
         @name = name
         @breed = breed
         @age = age
+        @@alldogs += 1
     end
+
+    def testself
+        bark
+    end
+
+    # Returns the class Dog
+    def self.countdogs
+        method2
+    end
+
+    def self.method2
+        puts("Method 2 class method")
+    end
+
+    # def self.testself
+    #     @@alldogs
+    # end
 
     def bark
         puts "Woof Woof!!!"
